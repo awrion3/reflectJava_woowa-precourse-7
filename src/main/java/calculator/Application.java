19d12e34b89ac631b9745b03be518a8e9061f641
RIWON <121172065+awrion3@users.noreply.github.com>
2024-12-01 16:11:33 +0900

    feat(application): add start point

---
+++ src/main/java/calculator/Application.java
@@ -0,0 +1,10 @@
+package calculator;
+
+import calculator.controller.Controller;
+
+public class Application {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.start();
+    }
+}
