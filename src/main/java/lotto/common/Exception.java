79e3d0671250bdbf2e834f0e89653b9a0a35c4a0
RIWON <121172065+awrion3@users.noreply.github.com>
2024-12-01 22:25:51 +0900

    feat(exception): add common exception

---
+++ src/main/java/lotto/common/Exception.java
@@ -0,0 +1,15 @@
+package lotto.common;
+
+public class Exception {
+    public static final String EMPTY_STRING = "[ERROR] 비어 있지 않은 문자열을 입력해야 합니다.";
+
+    public static final String NOT_INTEGER = "[ERROR] 숫자값은 정수형으로 입력해야 합니다.";
+    public static final String NOT_DOUBLE = "[ERROR] 숫자값은 실수형으로 입력해야 합니다.";
+    public static final String NOT_UNIQUE = "[ERROR] 중복되는 값이 없어야 합니다.";
+
+    public static final String INVALID_SIZE = "[ERROR] 로또 번호는 6개여야 합니다.";
+    public static final String INVALID_RANGE = "[ERROR] 값은 1 이상 45 이하의 값이어야 합니다.";
+
+    public static final String INVALID_AMOUNT = "[ERROR] 1000 이상의 값을 입력해야 합니다.";
+    public static final String INVALID_UNIT = "[ERROR] 1000 단위의 값으로 입력해야 합니다.";
+}
