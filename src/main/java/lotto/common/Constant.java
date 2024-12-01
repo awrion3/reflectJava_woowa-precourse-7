8aff87d73d3fa819a6da2867caddf6a4c0fcf430
RIWON <121172065+awrion3@users.noreply.github.com>
2024-12-01 22:15:46 +0900

    feat(constant): add common constant

---
+++ src/main/java/lotto/common/Constant.java
@@ -0,0 +1,15 @@
+package lotto.common;
+
+public class Constant {
+    public static final int ZERO_VALUE = 0;
+    public static final int ONE_VALUE = 1;
+    public static final int HUNDRED_VALUE = 100;
+
+    public static final int SIZE_LIMIT = 6;
+    public static final int AMOUNT_UNIT = 1000;
+
+    public static final int MIN_RANGE = 1;
+    public static final int MAX_RANGE = 45;
+
+    public static final String COMMA = ",";
+}
