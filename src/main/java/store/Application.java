b5c1674bccb162ffdc80682a4b67c1cabb4e4015
RIWON <121172065+awrion3@users.noreply.github.com>
2024-12-08 01:59:55 +0900

    feat(application): add start point

---
+++ src/main/java/store/Application.java
@@ -0,0 +1,10 @@
+package store;
+
+import store.controller.Controller;
+
+public class Application {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.start();
+    }
+}
