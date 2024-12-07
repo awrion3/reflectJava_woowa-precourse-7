3cb0a7f779f27a0f2ba62deffaeb4233ff02dd32
RIWON <121172065+awrion3@users.noreply.github.com>
2024-12-08 02:32:27 +0900

    feat(constant): add constants

---
+++ src/main/java/store/common/Constant.java
@@ -0,0 +1,16 @@
+package store.common;
+
+public class Constant {
+    public static final String REGEX = "\\[[가-힣]+-[0-9]+\\]";
+    public static final String DELIMITER = ",";
+    public static final char SEPARATOR = '-';
+
+    public static final int INDEX_ONE = 1;
+    public static final int INDEX_TWO = 2;
+    public static final int INDEX_THREE = 3;
+
+    public static final int VALUE_ZERO = 0;
+    public static final int VALUE_MAX = 8000;
+    public static final int VALUE_NEGATIVE = -1;
+    public static final double VALUE_PERCENT = 0.3;
+}
