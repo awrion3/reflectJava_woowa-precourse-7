8a0b9ab562a06be7d344b4635f4e5263b2e3ddd8
RIWON <121172065+awrion3@users.noreply.github.com>
2024-12-09 14:25:48 +0900

    test(orders): add validate quantity test case

--- src/test/java/store/model/OrdersTest.java
+++ src/test/java/store/model/OrdersTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class OrdersTest {
     @DisplayName("예외 테스트: 잘못된 형식으로 주문 상품과 수량을 입력한 경우")
     @ParameterizedTest
-    @ValueSource(strings = {"", ",", "cola-1", "콜라-1,사이다-1", "콜라-1.0", "콜라-999999", "탕수육-1"})
+    @ValueSource(strings = {"", ",", "cola-1", "콜라-1,사이다-1", "콜라-0", "콜라-1.0", "콜라-999999", "탕수육-1"})
     void check_validate_exception(String input) {
         Products products = new Products();
         assertThatThrownBy(() -> new Orders(input, products))
